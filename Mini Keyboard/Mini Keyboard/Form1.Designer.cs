c3e00d96c0271362c129de71f9d139038f3c274c
b6030684 <b6030684@my.shu.ac.uk>
2017-04-28 10:26:28 +0100

    Add files via upload
    Button 6 Complete Commit

--- Mini Keyboard/Mini Keyboard/Form1.Designer.cs
+++ Mini Keyboard/Mini Keyboard/Form1.Designer.cs
@@ -128,6 +128,7 @@
             this.button_6.TabIndex = 13;
             this.button_6.Text = "    6      MNO";
             this.button_6.UseVisualStyleBackColor = false;
+            this.button_6.Click += new System.EventHandler(this.button_6_Click);
             // 
             // button_5
             // 
--- Mini Keyboard/Mini Keyboard/Form1.cs
+++ Mini Keyboard/Mini Keyboard/Form1.cs
@@ -186,5 +186,28 @@ namespace Mini_Keyboard
 
             }
         }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            {
+                timer.Stop();
+                if (firstclick == true)
+                {
+                    listBox_global.Items.Clear();
+                    for (int i = 0; i < listBox_6.Items.Count; i++)
+                    {
+                        listBox_global.Items.Add(listBox_6.Items[i].ToString());
+
+                    }
+                    firstclick = false;
+
+                }
+
+                Numberofclicks++;
+
+                timer.Enabled = true;
+
+            }
+        }
     }
 }
